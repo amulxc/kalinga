ecb159842b557e4247ede6a564ab68400c53df1b
thej1812 <thejashwini1800@gmail.com>
2025-12-15 20:20:50 +0530

    career page

--- my-app/src/app/careers/page.jsx
+++ my-app/src/app/careers/page.jsx
@@ -7,7 +7,7 @@ import FaqSection from "../components/general/faq"
 import AutoBreadcrumb from "../components/layout/BreadcrumbData"
 
 const breadcrumbData = {
-  heroImage: "https://kalinga-university.s3.ap-south-1.amazonaws.com/campus-life/student2.webp",
+  heroImage: "https://kalinga-university.s3.ap-south-1.amazonaws.com/campus-life/career.webp",
   pageTitle: "Careers",
   customBreadcrumbs: [
     { label: 'Home', href: '/' },
@@ -50,7 +50,7 @@ export default function Careers() {
       <MainIntro  title="Why Work With Us?"
       description={["At Kalinga University, we believe that excellence begins with people. We offer a collaborative, growth-oriented environment where innovation, integrity, and inclusivity drive everything we do. Our faculty and staff are empowered with continuous learning opportunities, modern infrastructure, and a culture that values both academic and personal fulfillment.Join us and become part of a forward-thinking institution shaping the next generation of leaders through education, research, and innovation."
     ]}
-      imageUrl="https://kalinga-university.s3.ap-south-1.amazonaws.com/campus-life/student2.webp"
+      imageUrl="https://kalinga-university.s3.ap-south-1.amazonaws.com/campus-life/whywork.webp"
       imageAlt="Why Work With Us?" />
      
       <OpenPositions />
