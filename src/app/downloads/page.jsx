35bdd2cf659d200b37c98095ab9678f743847f9a
meiyarasu-xc <meiyarasu@xtracut.com>
2026-08-03 14:11:40 +0530

    chnages in forms

--- src/app/downloads/page.jsx
+++ src/app/downloads/page.jsx
@@ -12,24 +12,24 @@ const downloadsreport = [
   //   href: "https://cdn.kalingauniversity.ac.in/downloads/pharmacy_magazine.pdf",
   // },
   {
-    id: 8,
+    id: 0,
     text: "New Admission Form",
     href: "https://cdn.kalingauniversity.ac.in/downloads/newadmissionform.pdf",
   },
   {
     id: 1,
     text: "Application for the Issue of a Bonafide Certificate",
-    href: "https://cdn.kalingauniversity.ac.in/downloads/BONAFIDE+APPLICATION+FORM.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Bonafide-Application-Form.pdf",
   },
   {
     id: 2,
     text: "Application for the Issue of Semester / Year-wise Transcript",
-    href: "https://cdn.kalingauniversity.ac.in/nirf/application-for-issues-year-wise-transcipt.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-form-for-transcript-Certificate.pdf",
   },
   {
     id: 3,
     text: "Application for the Issue of a Migration Certificate",
-    href: "https://cdn.kalingauniversity.ac.in/downloads/APPLICATION_FORM_MIGRATION+CERTIFICATE.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-form-migration-Certificate.pdf",
   },
   {
     id: 4,
@@ -54,42 +54,42 @@ const downloadsreport = [
   {
     id: 9,
     text: "Application Form For the Issue of Medium of Instruction",
-    href: "https://cdn.kalingauniversity.ac.in/downloads/APPLICATION+FORM+FOR+ISSUE+OF+MEDIUM+OF+INSTRUCTION.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-Form-for-Issue-of-Medium-of-Instruction.pdf",
   },
   {
     id: 10,
     text: "Application Form For the Issue of a Letter of Recommendation",
-    href: "https://cdn.kalingauniversity.ac.in/downloads/APPLICATION+FORM+FOR+ISSUE+OF+LETTER+OF+RECOMMENDATION.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-Form-for-Issue-of-Letter-of-Recommendation.pdf",
   },
   {
     id: 11,
     text: "Application form for Name and Surname Correction",
-    href: "https://cdn.kalingauniversity.ac.in/downloads/Change_of_Name_and_Surname.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Change-of-Name-and-Surname.pdf",
   },
   {
     id: 12,
     text: "Application form for the Issue of Degree",
-    href: "https://cdn.kalingauniversity.ac.in/downloads/APPLICATION_FORM_DEGREE+CERTIFICATE.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-form-degree-Certificate.pdf",
   },
   {
     id: 13,
     text: "Application for Issue of Character Certificate",
-    href: "https://cdn.kalingauniversity.ac.in/downloads/CHARACTER_CERTIFICATE.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Character-certificate.pdf",
   },
   {
     id: 14,
     text: "Application form for Diploma",
-    href: "https://cdn.kalingauniversity.ac.in/downloads/APPLICATION_FORM_FOR_ISSUE_OF_DIPLOMA+CERTIFICATE.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-form-for-issue-of-diploma-Certificate.pdf",
   },
   {
     id: 15,
     text: "Application form for Transfer Certificate",
-    href: "https://cdn.kalingauniversity.ac.in/downloads/APPLICATION_FORM_TRANSFER.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-form-transfer-Certificate.pdf",
   },
   {
     id: 16,
     text: "Application form for Verification",
-    href: "https://cdn.kalingauniversity.ac.in/downloads/Verification+Form.pdf",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Verification-Form.pdf",
   },
   {
     id: 17,
@@ -106,6 +106,42 @@ const downloadsreport = [
     text: "Re-Totalling / Re-Evaluation Form",
     href: "https://cdn.kalingauniversity.ac.in/downloads/RE-TOTALING_%26_REEVALUATION_FORM.pdf",
   },
+  {
+    id: 19,
+    text: "Application For Issue of Duplicate Character Certificate",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-for-Duplicate-Character-certificate.pdf",
+  },
+  {
+    id: 20,
+    text: "Application Form For Issue of Duplicate Transfer Certificate",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-form-Duplicate-transfer-Certificate.pdf",
+  },
+  {
+    id: 21,
+    text: "Application Form For Issue of Duplicate Migration Certificate",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-form-Duplicate-Migration-Certificate.pdf",
+  },{
+    id: 22,
+    text: "Application Form For Issue of Duplicate Provisional Certificate",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Application-form-Duplicate-Provisional-Certificate.pdf",
+  },{
+    id: 20,
+    text: "Application Form For Issue of Attendance Certificate",
+    href: "https://cdn.kalingauniversity.ac.in/forms/Attendance+Certificate.docx",
+  },{
+    id: 20,
+    text: "",
+    href: "#",
+  },{
+    id: 20,
+    text: "",
+    href: "#",
+  },{
+    id: 20,
+    text: "",
+    href: "#",
+  },
+
 
 ];
 
